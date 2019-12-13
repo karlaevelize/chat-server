98b282d6ac57d72b43e9754cefe1727c691dd967
karaevelize <k.evelize@gmail.com>
2019-12-13 11:53:40 +0100

    Stream

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const Sse = require("json-sse");
+const Message = require("./message/model");
 const messageRouter = require("./message/router");
+
 const app = express();
 const port = 4000;
 
+const stream = new Sse();
+
 app.get("/", (request, response) => {
+  stream.send("hi");
   response.send("hello");
+});
+
+app.get("/stream", async (request, response, next) => {
+  try {
+    const messages = await Message.findAll();
+    stream.init(request, response);
+  } catch (error) {
+    next(error);
+  }
 });
 
 const jsonParser = bodyParser.json();
--- message/router.js
+++ message/router.js
@@ -2,16 +2,34 @@ const { Router } = require("express");
 const Message = require("./model");
 const router = new Router();
 
-router.get("/messages", (request, response, next) => {
-  Message.findAll()
-    .then(messages => response.send(messages))
-    .catch(errors => next(errors));
+// router.get("/messages", (request, response, next) => {
+//   Message.findAll()
+//     .then(messages => response.send(messages))
+//     .catch(errors => next(errors));
+// });
+
+// router.post("/messages", (request, response, next) => {
+//   Message.create(request.body)
+//     .then(message => response.send(message))
+//     .catch(errors => next(errors));
+// });
+
+router.get("/message", async (request, response, next) => {
+  try {
+    const messages = await Message.findAll();
+    response.send(messages);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post("/messages", (request, response, next) => {
-  Message.create(request.body)
-    .then(message => response.send(message))
-    .catch(errors => next(errors));
+router.post("/message", async (request, response, next) => {
+  try {
+    const message = await Message.create(request.body);
+    response.send(message);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
